93b2e93503ebef2f5cf18d004be4d29e257cc63b
Yuqoi <lolwolfyou@gmail.com>
2024-01-14 23:52:42 +0100

    initialized again database

--- managerapp/src/main/java/org/yuqoi/managerapp/utils/DatabaseConnector.java
+++ managerapp/src/main/java/org/yuqoi/managerapp/utils/DatabaseConnector.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class DatabaseConnector {
 
     private static final String URL = "jdbc:mysql://localhost:3306/jewelery_store";
-    private static final String USER = "rootg";
+    private static final String USER = "root";
     private static final String PASSWORD = "";
 
     public static Connection getConnection(){
