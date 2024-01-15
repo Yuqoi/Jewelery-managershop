4af5f2f7be32b8f12ec0e48777e6422014e82179
Yuqoi <lolwolfyou@gmail.com>
2024-01-15 16:51:12 +0100

    changed loginController

--- managerapp/src/main/java/org/yuqoi/managerapp/controllers/LoginController.java
+++ managerapp/src/main/java/org/yuqoi/managerapp/controllers/LoginController.java
@@ -15,6 +15,8 @@ import org.yuqoi.managerapp.utils.PasswordHasher;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
@@ -31,26 +33,39 @@ public class LoginController implements Initializable {
 
 
 
-    public void loginUser(ActionEvent event) {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
         loginBtn.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                String login = loginTextPanel.getText();
+
+                String name = loginTextPanel.getText();
                 String password = passwordTextPanel.getText();
 
                 String hashedPassword = PasswordHasher.passwordHasher(password);
 
-                Connection conn = DatabaseConnector.getConnection();
-                // TODO: make prepared statement to database to get login and password and match them with user
-                // TODO: if any of the textfields are null give them a warning
 
+                // TODO: Check if conn is null if yes then we cant run the app
+                // TODO: check if the given login name is valid and is in the database if not dont give an error
+                try {
+                    Connection conn = DatabaseConnector.getConnection();
+                    String sql = "SELECT * FROM login WHERE name == ?";
+                    PreparedStatement ps = conn.prepareStatement(sql);
+                    ps.setString(1, name);
+
+                    // execute given query
+                    ResultSet rs = ps.executeQuery();
+
+                    // TODO: WE HAVE TO check if the given name isn't null if yes then we cannot get any data
+                    // TODO: check if given name is in the database IF NOT then return wrong username/password alert
+
+
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
 
             }
         });
-    }
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
         minBtn.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
