35a4d33e62c310554a2dfb473a417c0b75f2d268
Yuqoi <lolwolfyou@gmail.com>
2024-01-18 20:09:12 +0100

    changing styles in inventory

--- managerapp/src/main/java/org/yuqoi/managerapp/controllers/windows/MainPanelController.java
+++ managerapp/src/main/java/org/yuqoi/managerapp/controllers/windows/MainPanelController.java
@@ -3,6 +3,7 @@ package org.yuqoi.managerapp.controllers.windows;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
@@ -12,20 +13,18 @@ import org.yuqoi.managerapp.utils.ScenePaths;
 import org.yuqoi.managerapp.utils.SceneSwitcher;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
 public class MainPanelController {
 
 
-    // all buttons in main panel
-    public Button inventoryBtn;
-    public Button manageBtn;
-    public Button salesBtn;
-    public Button invoiceBtn;
-    public Button settingsBtn;
 
-    // main panel to change views
     public Pane viewPanel;
+
+    private Button previousClickedButton;
+
 
     // Close window and minimize window functions
     public void closeWindow(ActionEvent event) {
@@ -35,6 +34,8 @@ public class MainPanelController {
         Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
         stage.setIconified(true);
     }
+
+
 
     public void btnInventory(MouseEvent mouseEvent) {
         SceneSwitcher.changeScene(viewPanel, ScenePaths.INVENTORYPANEL);
@@ -54,6 +55,19 @@ public class MainPanelController {
 
 
 
+    public void changeColors(ActionEvent event) {
+        Button clickedButton = (Button) event.getSource();
 
+        // Change the color of the clicked button
 
+        clickedButton.setStyle("-fx-background-color: red;");
+
+        // Revert the color of the previously clicked button (if any)
+        if (previousClickedButton != null && previousClickedButton != clickedButton) {
+            previousClickedButton.setStyle("-fx-background-color: white;");
+        }
+
+        // Update the reference to the previously clicked button
+        previousClickedButton = clickedButton;
+    }
 }
